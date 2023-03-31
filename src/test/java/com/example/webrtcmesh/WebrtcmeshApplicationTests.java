4e9b9dd58c5cae90073d26964d8956358aae36bc
potwings <ygk1995@naver.com>
2023-03-31 21:00:43 +0900

    Test

---
+++ src/test/java/com/example/webrtcmesh/WebrtcmeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrtcmesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcmeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
