2e438537d0e6db7b8c84b5c0171a89c5e3d31893
potwings <ygk1995@naver.com>
2023-04-03 22:35:09 +0900

    접속 후 요청 시 클라이언트들 리스트를 응답으로 전달해주도록 수정 TODO : 정상적인 채팅방 형식은 아니라고 봄 추가적인 수정 필요

--- src/main/java/com/example/webrtcmesh/signalling/handler/SignallingHandler.java
+++ src/main/java/com/example/webrtcmesh/signalling/handler/SignallingHandler.java
@@ -1,14 +1,12 @@
 package com.example.webrtcmesh.signalling.handler;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +16,7 @@ import java.util.Map;
 @Slf4j
 public class SignallingHandler extends TextWebSocketHandler {
 
-    Map<String, InetSocketAddress> addressList = new HashMap<>();
+    Map<String, List<String>> addressMap = new HashMap<>();
 
     /**
      * 클라이언트의 포트 정보는 지정해두고 사용하는게 맞고
@@ -26,17 +24,27 @@ public class SignallingHandler extends TextWebSocketHandler {
      */
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        InetSocketAddress remoteAddress = session.getRemoteAddress();
-        String id = (String)session.getAttributes().get("id");
-        addressList.put(id, remoteAddress);
+
+        String id = (String) session.getAttributes().get("id");
+        List<String> addressList = addressMap.get(id);
+        if (addressList == null) {
+            addressList = new ArrayList<>();
+        }
+        String address = session.getRemoteAddress().toString().substring(1);
+        addressList.add(address);
+        addressMap.put(id, addressList);
+        log.info("{} 채팅방에 {}가 접속", id, address);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 
         String callee = message.getPayload();
-        TextMessage textMessage = new TextMessage(addressList.get(callee).toString().substring(1));
+        Gson gson = new Gson();
+        List<String> addressList = addressMap.get(callee);
+        String json = gson.toJson(addressList);
+        TextMessage textMessage = new TextMessage(json);
         session.sendMessage(textMessage);
-        log.warn("{}로부터 {} 받음", session.getId(), message.getPayload());
+        log.info("{} 채팅방 접속 요청 : 클라이언트 목록 {}", message.getPayload(), json);
     }
 }
