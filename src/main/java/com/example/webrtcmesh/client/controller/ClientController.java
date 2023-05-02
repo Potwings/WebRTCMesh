c415f301d1bde78dd90a886e70cdcc882f468925
potwings <ygk1995@naver.com>
2023-05-02 22:55:21 +0900

    데모 클라이언트(https://www.baeldung.com/webrtc)를 참고하여 Google STUN서버와 연동하여 WebRTC 채팅 구현

--- src/main/java/com/example/webrtcmesh/client/controller/ClientController.java
+++ src/main/java/com/example/webrtcmesh/client/controller/ClientController.java
@@ -1,9 +1,17 @@
 package com.example.webrtcmesh.client.controller;
 
+import com.example.webrtcmesh.signalling.handler.SignallingHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ClientController {
 
     @GetMapping("/client")
@@ -11,5 +19,17 @@ public class ClientController {
 
     }
 
+    @GetMapping("/test")
+    public void testGet() {
+    }
+
+    @GetMapping("/list")
+    public void getRoomList(Model model){
+        List<WebSocketSession> sessionList = new ArrayList<>();
+        SignallingHandler.chatRoomMap.put("test1", sessionList);
+        SignallingHandler.chatRoomMap.put("test2", sessionList);
+        SignallingHandler.chatRoomMap.put("test3", sessionList);
+        model.addAttribute("roomList", SignallingHandler.chatRoomMap.keySet());
+    }
 
 }
---
+++ src/main/resources/static/client.js
@@ -0,0 +1,126 @@
+//connecting to our signaling server
+var conn = new WebSocket('ws://localhost:8080/signalling');
+console.log("js File load")
+conn.onopen = function() {
+    console.log("Connected to the signaling server");
+    initialize();
+};
+
+// 타 클라이언트로부터 메세지 수신
+conn.onmessage = function(msg) {
+    console.log("Got message", msg.data);
+    var content = JSON.parse(msg.data);
+    var data = content.data;
+    switch (content.event) {
+        // when somebody wants to call us
+        case "offer":
+            handleOffer(data);
+            break;
+        case "answer":
+            handleAnswer(data);
+            break;
+        // when a remote peer sends an ice candidate to us
+        case "candidate":
+            handleCandidate(data);
+            break;
+        default:
+            break;
+    }
+};
+
+function send(message) {
+    conn.send(JSON.stringify(message));
+}
+
+var peerConnection;
+var dataChannel;
+var input = document.getElementById("messageInput");
+
+function initialize() {
+    var configuration = {
+        "iceServers" : [ {
+            "url" : "stun:stun2.1.google.com:19302"
+        } ]
+    };
+    peerConnection = new RTCPeerConnection(configuration);
+
+    // Setup ice handling
+    peerConnection.onicecandidate = function(event) {
+        if (event.candidate) {
+            send({
+                event : "candidate",
+                data : event.candidate
+            });
+        }
+    };
+
+    // creating data channel
+    dataChannel = peerConnection.createDataChannel("dataChannel", {
+        reliable : true
+    });
+
+    dataChannel.onerror = function(error) {
+        console.log("Error occured on datachannel:", error);
+    };
+
+    // when we receive a message from the other peer, printing it on the console
+    dataChannel.onmessage = function(event) {
+        var data = event.data;
+        var newMessage = document.createElement('p');
+        // p 태그 안에 텍스트 값을 설정합니다.
+        newMessage.textContent = data;
+        document.querySelector('#messages').appendChild(newMessage);
+        console.log("message:", data);
+    };
+
+    dataChannel.onclose = function() {
+        console.log("data channel is closed");
+    };
+
+    peerConnection.ondatachannel = function (event) {
+        dataChannel = event.channel;
+    };
+
+}
+
+function createOffer() {
+    peerConnection.createOffer(function(offer) {
+        send({
+            event : "offer",
+            data : offer
+        });
+        peerConnection.setLocalDescription(offer);
+    }, function(error) {
+        alert("Error creating an offer");
+    });
+}
+
+function handleOffer(offer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    // create and send an answer to an offer
+    peerConnection.createAnswer(function(answer) {
+        peerConnection.setLocalDescription(answer);
+        send({
+            event : "answer",
+            data : answer
+        });
+    }, function(error) {
+        alert("Error creating an answer");
+    });
+
+};
+
+function handleCandidate(candidate) {
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+function handleAnswer(answer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    console.log("connection established successfully!!");
+};
+
+function sendMessage() {
+    dataChannel.send(input.value);
+    input.value = "";
+}
